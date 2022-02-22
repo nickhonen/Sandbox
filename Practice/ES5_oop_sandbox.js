ea50654c87b15675002c04521f4b41161e1a6e0a
Nicholas Honen <nickhonen@gmail.com>
2022-02-22 14:36:08 -0500

    did some async work, updated readme, changed directories around to make it more expandable/add more dedicated folders to reduce file tree clutter.

---
+++ Practice/ES5_oop_sandbox.js
@@ -0,0 +1,157 @@
+// REMINDER that classes still use prototypes and shit from ES5 as their logic
+
+// 1:Basic Literals
+
+const pokemon = { 
+    name: "Pikachu",
+    type: "electric",
+    generation: 1,
+    entry: function() {
+        return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+        Generation ${this.generation}.`
+    }
+};
+
+const pokemon2 = { 
+    name: "Cyndaquil",
+    type: "fire",
+    generation: 2,
+    entry: function() {
+        return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+        Generation ${this.generation}.`
+    }
+};
+
+// console.log(pokemon.entry());
+// console.log(Object.values(pokemon2));
+
+2: Constructor
+
+//Constructor Function
+function Pokemon(name, type, generation) {
+    this.name = name; 
+    this.type = type;
+    this.generation = generation;
+// Below is a good example of when to use a prototype method instead of a constructor
+    this.entry = function() {
+        return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+        Generation ${this.generation}.`;
+    }
+}
+// Instantiate Objects with Constructor
+const pokemon1 = new Pokemon("Pikachu", "electric", 1);
+const pokemon2 = new Pokemon("Cyndaquill", "fire", 2);
+
+console.log(pokemon1.entry());
+console.log(pokemon2);
+
+3: Prototypes
+
+/*If we want a name, type, generation with every pokemon but we dont want an 
+entry with every pokemon, we might want to make the entry function part of the 
+prototype instead of the constructor */
+
+
+function Pokemon(name, type, generation) {
+    const generations = [1998, 2000, 2003, 2007, 2011, 2013, 2016, 2019]
+    this.name = name; 
+    this.type = type;
+    this.generation = generation;
+}
+// Returns makeshift pokedex entry
+    Pokemon.prototype.entry = function() {
+        return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+        Generation ${this.generation}.`;
+    }
+// Returns how many years since the pokemon was released (in NA)
+    Pokemon.prototype.getAge = function() {
+        const generations = [1998, 2000, 2003, 2007, 2011, 2013, 2016, 2019]
+        const year = new Date().getFullYear();
+        const difference = year - generations[this.generation - 1];
+        return `${this.name} is ${difference} years old.`;
+    }
+// Changes type of pokemon
+    Pokemon.prototype.changeType = function(newType) {
+        this.type = newType;
+        this.updatedType = true;
+    }
+
+// Evolve pokemon TBD
+    Pokemon.prototype
+
+const pokemon1 = new Pokemon("Pikachu", "electric", 1);
+const pokemon2 = new Pokemon("Cyndaquill", "fire", 2);
+const pokemon3 = new Pokemon("Oshawott", Water, 5);
+
+console.log(pokemon1.entry());
+pokemon1.changeType("Fairy/Electric");
+console.log(pokemon1.entry());
+console.log(pokemon1.getAge());
+
+4: Inheritance
+
+// Constructor
+function Pokemon(name, type, generation) {
+    const generations = [1998, 2000, 2003, 2007, 2011, 2013, 2016, 2019]
+    this.name = name; 
+    this.type = type;
+    this.generation = generation;
+}
+// Returns makeshift pokedex entry
+    Pokemon.prototype.entry = function() {
+        return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+        Generation ${this.generation}.`;
+    }
+
+// NewPokemon Constructor
+function NewPokemon(name, type, generation, game) {
+    Pokemon.call(this, name, type, generation)
+    this.game = game;
+}
+
+// Inherit prototype
+NewPokemon.prototype = Object.create(Pokemon.prototype);
+
+// Instantiate new NewPokemon Object
+const newpokemon1 = new NewPokemon("Yveltal", "Dark/Flying", 6, "Pokemon X/Y");
+
+// Use the NewPokemon Constructor
+NewPokemon.prototype.constructor = NewPokemon;
+
+5: Object.create(prototype for object)
+
+    const pokemonProtos = {
+        entry: function() {
+            return `${this.name} is a/an ${this.type} type Pokemon, introduced in
+            Generation ${this.generation}.`;
+        }
+        getAge: function() {
+            const generations = [1998, 2000, 2003, 2007, 2011, 2013, 2016, 2019]
+            const year = new Date().getFullYear();
+            const difference = year - generations[this.generation - 1];
+            return `${this.name} is ${difference} years old.`;
+        }
+    }
+
+    const pokemon1 = Object.create(pokemonProtos);
+
+/* If I wanted to have writable, enumerable. configurable attribute options
+Object.create() would be good */
+
+let footballer = Object.create(Object.prototype,
+    {
+      name:{
+        value: 'Lio Messi',
+        configurable: false,
+        writable: false,
+        enumerable: false
+      },
+      club:{
+        value: 'FC Barcelona',
+        configurable: true,
+        writable: true,
+        enumerable: true
+      }
+    });
+  console.log(footballer.name)    //Output: Lio Messi
+  console.log(footballer.club)    //Output: FC Barcelona
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 This is going to be where I store all of the stuff I have made to ensure what I am learning sticks.
 Any random concept I learn about in a class or something that I just want to practice more I will throw in here. Hopefully seeing this repo expand all the time will be motivating. And when I look back it will be fun to see how far I've come. Also this a decent way for me to practice git.
 
+### CSPrep
 CSPrep is from a prep course that I took when I wanted to do a bootcamp. Was mainly learning javascript fundamentals.
 
-Webpages are my forray into HTML/CSS. Any webpage I mess around with that isn't big enough for a dedicated folder, or are just the result of a freeCodeCamp module I've done or something go here.
+### Webpages
+Webpages are my forray into HTML/CSS. Any webpage I mess around with that isn't big enough for a dedicated folder, or are just the result of a freeCodeCamp module I've done or something go here. Mainly meant to make my learning more active and ensure I am practicing building real sites while I'm learning through a class.
+
